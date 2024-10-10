6298825290ad39941767c3b7dc1a2d01af103d96
Steve Brown <brown2020@gmail.com>
2024-10-10 16:21:27 -0700

    header videos

--- src/data/mainBanner.ts
+++ src/data/mainBanner.ts
@@ -3,21 +3,27 @@ import { StaticImageData } from "next/image";
 
 // Define the type structure for each talk
 export interface MainBanner {
-    id: string;
-    source: string | StaticImageData;
-    type: "video" | "image";
+  id: string;
+  source: string | StaticImageData;
+  type: "video" | "image";
 }
 
 // Create the array of talks with the specified type
 export const mainBanner: MainBanner[] = [
-    {
-        id: "1",
-        source: "/assets/headerVideo.mp4",
-        type: "video",
-    },
-    {
-        id: "2",
-        source: headerImage,
-        type: "image",
-    },
+  {
+    id: "1",
+    source: headerImage,
+    type: "image",
+  },
+  {
+    id: "2",
+    source: "/assets/headerVideo1.mp4",
+    type: "video",
+  },
+
+  {
+    id: "3",
+    source: "/assets/headerVideo2.mp4",
+    type: "video",
+  },
 ];
