1236c35a14a3536917dc6ae604d0f644fb09a45d
Steve Brown <brown2020@gmail.com>
2025-04-27 21:29:00 -0700

    fix lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,22 @@ import "../components/components.css";
 import { Metadata } from "next";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Nunito } from "next/font/google";
+import { Nunito, Anek_Latin } from "next/font/google";
 import ScrollToTopButton from "@/components/ScrollToTopButton";
 import InitAos from "./components/utils/InitAos";
 
 const nunito = Nunito({
   subsets: ["latin"],
   weight: ["300", "400", "700"],
+  display: "swap",
+  variable: "--font-nunito",
+});
+
+const anekLatin = Anek_Latin({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  display: "swap",
+  variable: "--font-anek-latin",
 });
 
 export const metadata: Metadata = {
@@ -32,17 +41,9 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
         />
-        <link
-          href="https://fonts.googleapis.com/css?family=Zain"
-          rel="stylesheet"
-        ></link>
-        <link
-          href="https://fonts.googleapis.com/css?family=Anek Latin"
-          rel="stylesheet"
-        ></link>
       </head>
       <body
-        className={`${nunito.className} flex flex-col min-h-screen w-full text-slate-50`}
+        className={`${nunito.variable} ${anekLatin.variable} flex flex-col min-h-screen w-full text-slate-50`}
       >
         <InitAos />
         <div className="flex-1 master-contant">{children}</div>
